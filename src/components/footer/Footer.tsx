e644fb6cca3a58ccc16ea66cdab253f867f87170
Horacio Garzon <horaciorafaelg30@gmail.com>
2020-09-30 18:03:53 -0300

    removed tiwtter

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -8,8 +8,7 @@ const Footer: React.FC = () => {
     <Container>
       <div className='footer'>
         <Button className='footer-item btn-text' href={`${Config.etherscan}${Config.Token.address}`} target='_blank' >Z Contract</Button>
-        <Button className='footer-item btn-text' href='https://app.uniswap.org' target='_blank' >Uniswap Z-ETH</Button>
-        <Button className='footer-item btn-text' href='' target='_blank' >Twitter</Button>
+        <Button className='footer-item btn-text' href='https://uniswap.info/pair/0xf9f3e449ed119e0eac0d72c0b667363dd1539885' target='_blank' >Uniswap Z-ETH</Button>
         <Button className='footer-item btn-text' href='https://t.me/Ytoken' target='_blank' >Telegram</Button>
       </div>
     </Container>
